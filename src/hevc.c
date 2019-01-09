02cffcb2a77270797c5b86cd992a65dd7a8756d2
liyq <liyuanquan@qiniu.com>
2019-01-09 18:13:50 +0800

    save

--- src/hevc.c
+++ src/hevc.c
@@ -1,4 +1,4 @@
-// Last Update:2019-01-07 19:33:32
+// Last Update:2019-01-09 18:10:16
 /**
  * @file hevc.c
  * @brief 
@@ -8,8 +8,21 @@
  */
 
 #include <stdint.h>
+#include <string.h>
+#include "bs.h"
 
 #define MAX_SPATIAL_SEGMENTATION 4096 // max. value of u(12) field
+#define AV_INPUT_BUFFER_PADDING_SIZE 32
+#define NALU_MAX 16
+#define HEVC_MAX_SUB_LAYERS 7
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+typedef struct NalUnit {
+    uint8_t nalu_type;
+    uint8_t *addr;
+    int size;
+} NalUnit;
 
 typedef struct HVCCNALUnitArray {
     uint8_t  array_completeness;
@@ -41,12 +54,262 @@ typedef struct HEVCDecoderConfigurationRecord {
     HVCCNALUnitArray *array;
 } HEVCDecoderConfigurationRecord;
 
-
-int hevc_get_config( const uint8_t *data, int size, HEVCDecoderConfigurationRecord *config )
+static const uint8_t *hevc_find_startcode_internal(const uint8_t *p, const uint8_t *end)
 {
-    if ( !data || !config ) {
+    const uint8_t *a = p + 4 - ((intptr_t)p & 3);
+
+    for (end -= 3; p < a && p < end; p++) {
+        if (p[0] == 0 && p[1] == 0 && p[2] == 1)
+            return p;
+    }
+
+    for (end -= 3; p < end; p += 4) {
+        uint32_t x = *(const uint32_t*)p;
+        if ((x - 0x01010101) & (~x) & 0x80808080) { // generic
+            if (p[1] == 0) {
+                if (p[0] == 0 && p[2] == 1)
+                    return p;
+                if (p[2] == 0 && p[3] == 1)
+                    return p+1;
+            }
+            if (p[3] == 0) {
+                if (p[2] == 0 && p[4] == 1)
+                    return p+2;
+                if (p[4] == 0 && p[5] == 1)
+                    return p+3;
+            }
+        }
+    }
+
+    for (end += 3; p < end; p++) {
+        if (p[0] == 0 && p[1] == 0 && p[2] == 1)
+            return p;
+    }
+
+    return end + 3;
+}
+
+const uint8_t *hevc_find_startcode( const uint8_t *p, const uint8_t *end )
+{
+    const uint8_t *out= hevc_find_startcode_internal(p, end);
+
+    if(p<out && out<end && !out[-1]) out--;
+
+    return out;
+}
+
+static void hevc_update_ptl(HEVCDecoderConfigurationRecord *config,
+                            HVCCProfileTierLevel *ptl)
+{
+    config->general_profile_space = ptl->profile_space;
+    if (config->general_tier_flag < ptl->tier_flag)
+        config->general_level_idc = ptl->level_idc;
+    else
+        config->general_level_idc = FFMAX(hvcc->general_level_idc, ptl->level_idc);
+    config->general_tier_flag = MAX(hvcc->general_tier_flag, ptl->tier_flag);
+    config->general_profile_idc = MAX(hvcc->general_profile_idc, ptl->profile_idc);
+    config->general_profile_compatibility_flags &= ptl->profile_compatibility_flags;
+    config->general_constraint_indicator_flags &= ptl->constraint_indicator_flags;
+}
+
+static void hevc(bs_t *bs, HEVCDecoderConfigurationRecord *config,
+                 unsigned int max_sub_layers_minus1)
+{
+    unsigned int i;
+    HVCCProfileTierLevel general_ptl;
+    uint8_t sub_layer_profile_present_flag[HEVC_MAX_SUB_LAYERS];
+    uint8_t sub_layer_level_present_flag[HEVC_MAX_SUB_LAYERS];
+
+    general_ptl.profile_space               = bs_read_u( bs, 2);
+    general_ptl.tier_flag                   = bs_read_u1(bs);
+    general_ptl.profile_idc                 = bs_read_u( bs, 5);
+    general_ptl.profile_compatibility_flags = get_bits_long(gb, 32);
+    general_ptl.constraint_indicator_flags  = get_bits64(gb, 48);
+    general_ptl.level_idc                   = get_bits(gb, 8);
+    hvcc_update_ptl(hvcc, &general_ptl);
+
+    for (i = 0; i < max_sub_layers_minus1; i++) {
+        sub_layer_profile_present_flag[i] = get_bits1(gb);
+        sub_layer_level_present_flag[i]   = get_bits1(gb);
+    }
+
+    if (max_sub_layers_minus1 > 0)
+        for (i = max_sub_layers_minus1; i < 8; i++)
+            skip_bits(gb, 2); // reserved_zero_2bits[i]
+
+    for (i = 0; i < max_sub_layers_minus1; i++) {
+        if (sub_layer_profile_present_flag[i]) {
+            skip_bits_long(gb, 32);
+            skip_bits_long(gb, 32);
+            skip_bits     (gb, 24);
+        }
+
+        if (sub_layer_level_present_flag[i])
+            skip_bits(gb, 8);
+    }
+}
+
+static int hevc_parse_vps( bs_t *bs, int size, HEVCDecoderConfigurationRecord *config )
+{
+    unsigned int vps_max_sub_layers_minus1;
+
+    bs_skip_u( bs, 12 );
+
+    vps_max_sub_layers_minus1 = bs_read_u( bs, 3 );
+
+    config->numTemporalLayers = MAX(config->numTemporalLayers, vps_max_sub_layers_minus1 + 1);
+
+    bs_skip_u( bs, 17 );
+
+    return 0;
+err:
+    return -1;
+}
+
+static int hevc_parse_pps( bs_t *bs, int size, HEVCDecoderConfigurationRecord *config )
+{
+    return 0;
+err:
+    return -1;
+}
+
+static int hevc_parse_sps( bs_t *bs, int size, HEVCDecoderConfigurationRecord *config )
+{
+    return 0;
+err:
+    return -1;
+}
+
+int hevc_parse_nalu( const uint8_t *data_in, int size, NalUnit *nalu_list )
+{
+    int i = 0;
+    const uint8_t *p = data_in;
+    const uint8_t *end = p + size;
+    const uint8_t *nal_start = NULL, *nal_end = NULL;
+    uint8_t nalu_type = 0;
+
+    nal_start = hevc_find_startcode(p, end);
+    for (;;) {
+        while (nal_start < end && !*(nal_start++));
+        if (nal_start == end)
+            break;
+
+        nal_end = hevc_find_startcode(nal_start, end);
+        nalu_type = (nal_start[4] >> 1) & 0x3f;
+        if ( nalu_type == HEVC_NAL_VPS ||
+             nalu_type == HEVC_NAL_SPS ||
+             nalu_type == HEVC_NAL_PPS ||
+             nalu_type == HEVC_NAL_SEI_PREFIX ||
+             nalu_type == HEVC_NAL_SEI_SUFFIX ) {
+            nalu_list[i].nalu_type = nalu_type;
+            nalu_list[i].size = nal_end - nal_start;
+            nalu_list[i++].addr = nal_start;
+        }
+        nal_start = nal_end;
+    }
+
+    return i;
+}
+
+static uint8_t *nalu_extract_rbsp(const uint8_t *src, uint32_t src_len,
+                                      uint32_t *dst_len)
+{
+    uint8_t *dst;
+    uint32_t i, len;
+
+    dst = malloc(src_len + AV_INPUT_BUFFER_PADDING_SIZE);
+    if (!dst)
+        return NULL;
+
+    /* NAL unit header (2 bytes) */
+    i = len = 0;
+    while (i < 2 && i < src_len)
+        dst[len++] = src[i++];
+
+    while (i + 2 < src_len)
+        if (!src[i] && !src[i + 1] && src[i + 2] == 3) {
+            dst[len++] = src[i++];
+            dst[len++] = src[i++];
+            i++; // remove emulation_prevention_three_byte
+        } else
+            dst[len++] = src[i++];
+
+    while (i < src_len)
+        dst[len++] = src[i++];
+
+    *dst_len = len;
+    return dst;
+}
+
+int hevc_get_config( const uint8_t *data_in, int size, HEVCDecoderConfigurationRecord *config )
+{
+    int ret = 0, i = 0;
+    NalUnit nalu_list[NALU_MAX];
+
+    if ( !data_in || !config ) {
         goto err;
     }
+    
+    memset( nalu_list, 0, sizeof(nalu_list) );
+    ret = hevc_parse_nalu( data_in, size, nalu_list );
+    if ( ret <= 0 ) {
+        goto err; 
+    }
+
+    for ( i=0; i<ret; i++ ) {
+        uint32_t rbsp_size = 0;
+        uint8_t *rbsp_buf = NULL;
+        bs_t *bs = NULL;
+
+        if ( nalu_list[i].nalu_type != HEVC_NAL_VPS &&
+             nalu_list[i].nalu_type != HEVC_NAL_SPS &&
+             nalu_list[i].nalu_type != HEVC_NAL_PPS ) {
+            continue;
+        }
+
+        rbsp_buf = nalu_extract_rbsp( nalu_list[i].addr, nalu_list[i].size, &rbsp_size );
+        if ( !rbsp_buf || rbsp_size <= 0 ) {
+            goto err;
+        }
+
+        rbsp_buf += 2;// skip nal unit header,2bytes
+        bs = bs_new( rbsp_buf, rbsp_size );
+        if ( !bs ) {
+            goto err;
+        }
+
+        switch( nalu_list[i].nalu_type ) {
+        case HEVC_NAL_VPS:
+            ret = hevc_parse_vps( bs, config );
+            if ( ret < 0 ) {
+                free( rbsp_buf );
+                goto err;
+            }
+            free( rbsp_buf );
+            break;
+        case HEVC_NAL_SPS:
+            ret = hevc_parse_sps( bs, config );
+            if ( ret < 0 ) {
+                free( rbsp_buf );
+                goto err;
+            }
+            free( rbsp_buf );
+            break;
+        case HEVC_NAL_PPS:
+            ret = hevc_parse_pps( bs, config );
+            if ( ret < 0 ) {
+                free( rbsp_buf );
+                goto err;
+            }
+            free( rbsp_buf );
+            break;
+        default:
+            free( rbsp_buf );
+            break;
+        }
+    }
+
+    return 0;
 
 err:
     return -1;
---
+++ src/hevc.h
@@ -0,0 +1,42 @@
+// Last Update:2019-01-09 11:08:23
+/**
+ * @file hevc.h
+ * @brief 
+ * @author felix
+ * @version 0.1.00
+ * @date 2019-01-09
+ */
+
+#ifndef HEVC_H
+#define HEVC_H
+
+typedef enum HEVCNALUnitType {
+    HEVC_NAL_TRAIL_N    = 0,
+    HEVC_NAL_TRAIL_R    = 1,
+    HEVC_NAL_TSA_N      = 2,
+    HEVC_NAL_TSA_R      = 3,
+    HEVC_NAL_STSA_N     = 4,
+    HEVC_NAL_STSA_R     = 5,
+    HEVC_NAL_RADL_N     = 6,
+    HEVC_NAL_RADL_R     = 7,
+    HEVC_NAL_RASL_N     = 8,
+    HEVC_NAL_RASL_R     = 9,
+    HEVC_NAL_BLA_W_LP   = 16,
+    HEVC_NAL_BLA_W_RADL = 17,
+    HEVC_NAL_BLA_N_LP   = 18,
+    HEVC_NAL_IDR_W_RADL = 19,
+    HEVC_NAL_IDR_N_LP   = 20,
+    HEVC_NAL_CRA_NUT    = 21,
+    HEVC_NAL_VPS        = 32,
+    HEVC_NAL_SPS        = 33,
+    HEVC_NAL_PPS        = 34,
+    HEVC_NAL_AUD        = 35,
+    HEVC_NAL_EOS_NUT    = 36,
+    HEVC_NAL_EOB_NUT    = 37,
+    HEVC_NAL_FD_NUT     = 38,
+    HEVC_NAL_SEI_PREFIX = 39,
+    HEVC_NAL_SEI_SUFFIX = 40,
+} HEVCNALUnitType;
+
+
+#endif  /*HEVC_H*/
